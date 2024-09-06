2d3824708fa537a0358f4f0864e1648add76b02f
rofe33 <r3veal@protonmail.ch>
2024-09-06 16:42:10 +0300

    Updating PasswordManagerInterface

--- app/src/main/java/com/raphaeltannous/PasswordManagerInterface.java
+++ app/src/main/java/com/raphaeltannous/PasswordManagerInterface.java
@@ -8,7 +8,29 @@ import java.util.List;
 public interface PasswordManagerInterface {
 
     public List<String[]> readPasswords();
-    public void addPassword(String website, String username, String password);
-    public void modifyPassword(int passwordIndex, String newPassword);
-    public void deletePassword(int passwordIndex);
+
+    // Website/Username/Password are required to be in a password.
+    public void addPassword(
+        String website,
+        String username,
+        String password
+    );
+
+    public void modifyWebsite(int passwordId, String newWebsite); // newWebsite cannot be empty
+    public void modifyUsername(int passwordId, String newUsername); // newUsername cannot be empty
+    public void modifyPassword(int passwordId, String newPassword); // newPassword cannot be empty
+
+    public void addOTP(int passwordId, String otp); // otp cannot be empty if we are adding OTP
+    public void modifyOTP(int passwordId, String newOTP); // if newOTP is empty, then remove OTP
+
+    public void addNote(int passwordId, String note); // note cannot be empty if we are adding a note
+    public void modifyNote(int passwordId, String newNote); // if newNote is empty, then remove note
+
+    public void deletePassword(int passwordId);
+
+    public void addBackupCode(int passwordId, String backupCode); // backupCode cannot be empty if we are adding a backup code
+    public void removeBackupCode(int backupCodeId);
+    public void setBackupCodeUsed(int backupCodeId);
+
+    public void removePassword(int passwordId);
 }
