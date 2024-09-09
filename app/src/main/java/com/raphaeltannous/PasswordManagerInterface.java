0cb13a544c956801fd13a966d5a59fe0da545fbe
rofe33 <r3veal@protonmail.ch>
2024-09-09 20:25:16 +0300

    Updating PasswordManagerInterface

--- app/src/main/java/com/raphaeltannous/PasswordManagerInterface.java
+++ app/src/main/java/com/raphaeltannous/PasswordManagerInterface.java
@@ -7,7 +7,7 @@ import java.util.List;
  */
 public interface PasswordManagerInterface {
 
-    public List<String[]> readPasswords();
+    public List<String[]> fetchPasswords();
 
     // Website/Username/Password are required to be in a password.
     public void addPassword(
@@ -20,17 +20,17 @@ public interface PasswordManagerInterface {
     public void modifyUsername(int passwordId, String newUsername); // newUsername cannot be empty
     public void modifyPassword(int passwordId, String newPassword); // newPassword cannot be empty
 
-    public void addOTP(int passwordId, String otp); // otp cannot be empty if we are adding OTP
-    public void modifyOTP(int passwordId, String newOTP); // if newOTP is empty, then remove OTP
-
-    public void addNote(int passwordId, String note); // note cannot be empty if we are adding a note
-    public void modifyNote(int passwordId, String newNote); // if newNote is empty, then remove note
-
     public void deletePassword(int passwordId);
 
+    public String fetchOTP(int passwordId);
+    public void modifyOTP(int passwordId, String newOTP); // if newOTP is empty, then remove OTP
+
+    public String fetchNote(int passwordId); // note doesn't exists if note is ""
+    public void modifyNote(int passwordId, String newNote); // if newNote is empty, then remove note
+
+    public List<String[]> fetchBackupCodes(int passwordId);
     public void addBackupCode(int passwordId, String backupCode); // backupCode cannot be empty if we are adding a backup code
     public void removeBackupCode(int backupCodeId);
-    public void setBackupCodeUsed(int backupCodeId);
-
-    public void removePassword(int passwordId);
+    public void updateBackupCodeStatus(int backupCodeId, int status); // status is 0 if backup code is not used otherwise 1.
+    public void updateHasBackupCodeStatus(int passwordId, int status);
 }
