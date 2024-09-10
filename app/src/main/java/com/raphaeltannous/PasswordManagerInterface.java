62f876ce133076dbae9b4ccd9320869de71c507f
Raphael Tannous <raphaeltannous@proton.me>
2024-09-10 16:20:52 +0300

    Removing unnecessary imports

--- app/src/main/java/com/raphaeltannous/PasswordManagerInterface.java
+++ app/src/main/java/com/raphaeltannous/PasswordManagerInterface.java
@@ -2,7 +2,6 @@ package com.raphaeltannous;
 
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
