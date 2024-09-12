49049914cbd26e2467333a4264e90c231e1628eb
rofe33 <r3veal@protonmail.ch>
2024-09-12 19:09:11 +0300

    Implementing different functions in the EPMOpenPanel

--- app/src/main/java/com/raphaeltannous/EPMOpenPanel.java
+++ app/src/main/java/com/raphaeltannous/EPMOpenPanel.java
@@ -1,11 +1,19 @@
 package com.raphaeltannous;
 
+import java.awt.Color;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Objects;
+
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import com.formdev.flatlaf.FlatClientProperties;
+import com.formdev.flatlaf.FlatLaf;
 
 import net.miginfocom.swing.MigLayout;
 
@@ -15,7 +23,17 @@ import net.miginfocom.swing.MigLayout;
 public class EPMOpenPanel extends JPanel {
     EPMFrame frame;
 
+    private JLabel authenticationLabel;
+    private JLabel filePathLabel;
+
     private JPasswordField passwordField;
+
+    private JButton chooseFilePathButton;
+    private JButton openDatabaseButton;
+
+    private Path chosenFile = null;
+
+    private Color[] success = new Color[]{new Color(0, 255, 0), new Color(200, 255, 200)};
 
     EPMOpenPanel(
         EPMFrame frame
@@ -33,12 +51,13 @@ public class EPMOpenPanel extends JPanel {
 
         JLabel passwordLabel = new JLabel();
 
-        JLabel filePathLabel = new JLabel();
+        filePathLabel = new JLabel();
+        authenticationLabel = new JLabel();
 
         passwordField = new JPasswordField();
 
-        JButton chooseFilePathButton = new JButton();
-        JButton openDatabaseButton = new JButton();
+        chooseFilePathButton = new JButton();
+        openDatabaseButton = new JButton();
 
         setLayout(new MigLayout());
 
@@ -55,22 +74,88 @@ public class EPMOpenPanel extends JPanel {
         add(passwordLabel, "cell 0 2");
 
         passwordField.setColumns(20);
+        EPMUtilities.addChangeListener(passwordField, e -> passwordFieldChangeListener());
         add(passwordField, "cell 1 2");
 
         openDatabaseButton.setText("Open Database");
         openDatabaseButton.setMnemonic('O');
         openDatabaseButton.setDisplayedMnemonicIndex(0);
+        openDatabaseButton.setEnabled(false);
         openDatabaseButton.addActionListener(e -> openActionListener());
         add(openDatabaseButton, "cell 0 3 2 1,align center");
+
+        add(authenticationLabel, "cell 0 4 2 1, align center");
 	}
 
-	private Object openActionListener() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'openActionListener'");
+    private boolean passwordConfirmation() {
+        if (passwordField.getPassword().length > 0) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private void passwordFieldChangeListener() {
+        if (!passwordConfirmation()) {
+            passwordField.putClientProperty("JComponent.outline", "error");
+        } else {
+            passwordField.putClientProperty("JComponent.outline", this.success);
+        }
+
+        checkOpenButtonStatus();
 	}
 
-	private Object chooseActionListener() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'chooseActionListener'");
+	private void checkOpenButtonStatus() {
+        if (
+            passwordField.getPassword().length > 0 &&
+            Objects.nonNull(chosenFile)
+        ) {
+            openDatabaseButton.setEnabled(true);
+            return;
+        }
+
+        openDatabaseButton.setEnabled(false);
+    }
+
+	private void openActionListener() {
+        if (
+            PasswordManagerInterface.isFileADB(
+                chosenFile,
+                String.valueOf(passwordField.getPassword())
+            )
+        ) {
+
+            frame.contentPane.removeAll();
+            FlatLaf.revalidateAndRepaintAllFramesAndDialogs();
+
+            return;
+        }
+
+        passwordField.putClientProperty("JComponent.outline", "error");
+
+        authenticationLabel.setText("Wrong Credentials...");
+        authenticationLabel.setForeground(Color.RED);
+
+        // TODO: Change screen when Implemented
+	}
+
+	private void chooseActionListener() {
+        JFileChooser fileChooser = new JFileChooser();
+
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Encrypt Password Manager Files", "epm");
+
+        fileChooser.setFileFilter(filter);
+
+        int result = fileChooser.showOpenDialog(this);
+
+        if (result == JFileChooser.APPROVE_OPTION) {
+            chosenFile = Paths.get(fileChooser.getSelectedFile().getAbsolutePath());
+
+            chosenFile = chosenFile.normalize();
+
+            filePathLabel.setText(chosenFile.toString());
+        }
+
+        checkOpenButtonStatus();
 	}
 }
